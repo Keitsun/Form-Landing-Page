4e91a30e0d5f1ebb2f9ad216ce40aebdfe7126c7
Keitsun <ashirogimoto109@gmail.com>
2020-09-17 12:56:59 +0200

    Commit 1 for form

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.querySelector(".img-btn").addEventListener("click", function()
+// {
+// 	document.querySelector(".conatainer").classList.toggle("s-signup")
+// }
+// );
+
+document.querySelector('.img-btn').addEventListener('click', function()
+	{
+		document.querySelector('.container').classList.toggle('s-signup')
+	}
+);
